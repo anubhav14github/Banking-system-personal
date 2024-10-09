73ca0c097bf3344487216c51f1c620ad53e15653
Anubhav Mishra <mishra.anubhav1409@gmail.com>
2024-10-09 23:45:53 +0100

    Implement try 3

--- components/Try2.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Try2 = () => {
-  return (
-    <div>Try2</div>
-  )
-}
-
-export default Try2
---
+++ components/Try3.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Try3 = () => {
+  return (
+    <div>Try3</div>
+  )
+}
+
+export default Try3
